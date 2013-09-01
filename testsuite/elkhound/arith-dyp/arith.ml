945c3f6d5c221221277cce11c25c4e0f605bb326
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-09-01 09:27:10 +0200

    add dypgen based arith parser

---
+++ testsuite/elkhound/arith-dyp/arith.ml
@@ -0,0 +1,12 @@
+let main =
+  List.iter (fun file ->
+    let input = open_in file in
+    let lexbuf = Lexing.from_channel input in
+
+    Printf.printf "%d\n" (fst (List.hd (ArithParser.parse ArithLexer.token lexbuf)))
+  )
+
+
+let () =
+  Printexc.record_backtrace true;
+  Cmdline.run main
