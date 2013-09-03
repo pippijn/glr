d9cd16309bbf1c09f92617d5fd62e99e3374b486
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-09-03 07:11:10 +0200

    cosmetics

--- src/gramanl/grammarGraph.ml
+++ src/gramanl/grammarGraph.ml
@@ -4,18 +4,18 @@ module G = Nonterminal.Graph
 
 
 module Dot = Graph.Graphviz.Dot(struct
-    include G
-    let graph_attributes _ = []
-    let default_vertex_attributes _ = []
+  include G
+  let graph_attributes _ = []
+  let default_vertex_attributes _ = []
 
-    let vertex_name nonterm = Sloc.value nonterm.nbase.name
-    let vertex_attributes _ = []
+  let vertex_name nonterm = Sloc.value nonterm.nbase.name
+  let vertex_attributes _ = []
 
-    let get_subgraph _ = None
+  let get_subgraph _ = None
 
-    let default_edge_attributes _ = []
-    let edge_attributes _ = []
-  end)
+  let default_edge_attributes _ = []
+  let edge_attributes _ = []
+end)
 
 let dot formatter g =
   Dot.fprint_graph formatter g
--- src/gramanl/stateGraph.ml
+++ src/gramanl/stateGraph.ml
@@ -2,18 +2,18 @@ open AnalysisEnvType
 
 
 module Dot = Graph.Graphviz.Dot(struct
-    include ItemSet.Graph
-    let graph_attributes _ = []
-    let default_vertex_attributes _ = []
+  include ItemSet.Graph
+  let graph_attributes _ = []
+  let default_vertex_attributes _ = []
 
-    let vertex_name state = "S" ^ Ids.State.to_string state.state_id
-    let vertex_attributes _ = []
+  let vertex_name state = "S" ^ Ids.State.to_string state.state_id
+  let vertex_attributes _ = []
 
-    let get_subgraph _ = None
+  let get_subgraph _ = None
 
-    let default_edge_attributes _ = []
-    let edge_attributes _ = []
-  end)
+  let default_edge_attributes _ = []
+  let edge_attributes _ = []
+end)
 
 let dot formatter g =
   Dot.fprint_graph formatter g
